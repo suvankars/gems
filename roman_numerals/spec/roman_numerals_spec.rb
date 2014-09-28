d087b2520dd997707d366548bff871aeab7c921a
suvankar <suvankar.17@gmail.com>
2014-09-28 10:03:06 +0530

    adding some more test case to handel convertion roman to roman and decimal to decimal

--- roman_numerals/spec/roman_numerals_spec.rb
+++ roman_numerals/spec/roman_numerals_spec.rb
@@ -1,24 +1,24 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require 'roman_numeral'
+require 'roman-numeral'
 
 describe RomanNumeral do
-    base_digits = {
-	1    => 'I',
-	4    => 'IV',
-	5    => 'V',
-	9    => 'IX',
-	10   => 'X',
-	40   => 'XL',
-	50   => 'L',
-	90   => 'XC',
-	100  => 'C',
-	400  => 'CD',
-	500  => 'D',
-	900  => 'CM',
-	1000 => 'M'
-    }
+	base_digits = {
+		1    => 'I',
+		4    => 'IV',
+		5    => 'V',
+		9    => 'IX',
+		10   => 'X',
+		40   => 'XL',
+		50   => 'L',
+		90   => 'XC',
+		100  => 'C',
+		400  => 'CD',
+		500  => 'D',
+		900  => 'CM',
+		1000 => 'M'
+	}
 
     describe ".valid?" do
 	it "should find wheather an input belongs to 7 valid roman numeral" do
@@ -74,43 +74,67 @@ describe RomanNumeral do
     
 
     describe ".to_decimal" do
-	base_digits.each do |decimal, roman|
-	    it "should convert the roman numeral #{roman} to the decimal value #{decimal}" do
-		numeral = RomanNumeral.new( roman )
-		numeral.to_decimal.should == decimal
-	    end
-	end
+		base_digits.each do |decimal, roman|
+		    it "should convert the roman numeral #{roman} to the decimal value #{decimal}" do
+			numeral = RomanNumeral.new( roman )
+			numeral.to_decimal.should == decimal
+		    end
+		end
 
-	describe ".to_roman" do
-	base_digits.each do |decimal, roman|
-	    it "should convert the decimal value #{decimal} to the roman numeral #{roman}" do
-		numeral = RomanNumeral.new( decimal )
-		numeral.to_roman.should == roman
-	    end
-	end
-    end
-
-	numerals = { 'MCMXLIV' => 1944,
+		numerals = { 'MCMXLIV' => 1944,
 		     'IX' => 9, 
 		     'DLIX' => 559,
 		     'XI' => 11,
 		     'xvii' => 17
 		   }
 
-	numerals.each do |roman, decimal|
-	    it "should convert the roman numeral #{roman} to the decimal value #{decimal}" do
-		numeral = RomanNumeral.new( roman )
-		numeral.to_decimal.should == decimal
-	    end
+		numerals.each do |roman, decimal|
+		    it "should convert the roman numeral #{roman} to the decimal value #{decimal}" do
+			numeral = RomanNumeral.new( roman )
+			numeral.to_decimal.should == decimal
+		    end
+		end
+
 	end
 
-	numerals = ['VX', 'LL', 'IIII', 'VVLDD', 'IDD']
-	numerals.each do |roman|
-	    it "should NOT convert the roman numeral #{roman} to the decimal value " do
-		numeral = RomanNumeral.new( roman )
-		numeral.to_decimal.should == "Invalid roman numerals"
-	    end
-	end
+	describe ".to_roman" do
+		base_digits.each do |decimal, roman|
+		    it "should convert the int decimal value #{decimal} to the roman numeral #{roman}" do
+			numeral = RomanNumeral.new( decimal )
+			numeral.to_roman.should == roman
+		    end
+		end
+    	
+    	it "should convert string decimal value to roman numeral" do
+			numeral = RomanNumeral.new( "4" )
+			numeral.to_roman.should == "IV"			    		
+    	end
+
+		numerals = ['VX', 'LL', 'IIII', 'VVLDD', 'IDD']
+		numerals.each do |roman|
+		    it "should NOT convert the roman numeral #{roman} to the decimal value " do
+				numeral = RomanNumeral.new( roman )
+				numeral.to_decimal.should == "Invalid numeral #{roman}"
+		    end
+		end
     end
-    
+
+    describe "Conversion from Roman to Decimal and viceversa" do
+    	it "should convert roman to roman" do
+    		numeral = RomanNumeral.new( "IV" )
+    		numeral.to_roman.should == "IV"
+    	end
+
+    	it "should convert decimal to decimal" do
+    		numeral = RomanNumeral.new( 4 )
+    		numeral.to_decimal.should == 4
+    		numeral = RomanNumeral.new( "4")
+    		numeral.to_decimal.should == "4"
+    	end
+
+    	it "should not convert to any value" do
+    		numeral = RomanNumeral.new( "4IV" )
+    		numeral.to_roman.should == "Invalid numeral 4IV"
+    	end
+    end 
 end
